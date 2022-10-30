6127456f7748b53ec8ad705a763f09b2be3f8fef
Visu <744855810@qq.com>
2022-10-30 18:50:05 +0800

    Add epsilon to trend loss to avoid divide by zero

--- models/informer/trendloss.py
+++ models/informer/trendloss.py
@@ -4,13 +4,14 @@ import torch.nn.functional as F
 
 class TrendLoss(torch.nn.Module):
 
-    def __init__(self, c=0.1, back_bone=F.mse_loss):
+    def __init__(self, c=0.1, back_bone=F.mse_loss, eps=1e-8):
         super(TrendLoss, self).__init__()
         self.c = c
         self.back_bone = back_bone
+        self.eps = eps
 
     def forward(self, yhat, y):
         assert yhat.shape[-1] == 1
         back_bone_loss = self.back_bone(yhat, y)
-        coef = 1 + self.c * torch.mean(torch.abs(yhat) / yhat - torch.abs(y) / y)
+        coef = 1 + self.c * torch.mean(torch.abs(yhat) / (yhat + self.eps) - torch.abs(y) / (y + self.eps))
         return coef * back_bone_loss
